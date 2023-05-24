b477882d41b37bc657a4b78e020e809d544b4abf
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-05-24 15:33:55 +0500

    header added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
+import "./Header.css";
+import { NavLink, useLocation } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  const localtion = useLocation();
+  if (localtion.pathname === "/login") return;
+  return (
+    <header>
+      <NavLink className="logo" to="/">
+        Quiz app
+      </NavLink>
+      <nav>
+        <NavLink className="profile" to="/profile">
+          DA
+        </NavLink>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
