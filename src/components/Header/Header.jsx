b9754acdc13e9f4c611ad24980d2819afa68c390
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-05-26 14:57:18 +0500

    header was hidden on the main page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,25 @@
-import React, { useRef } from "react";
+import React from "react";
 import "./Header.css";
-import { NavLink, useLocation } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 
 const Header = () => {
-  const localtion = useLocation();
-  if (localtion.pathname === "/login") return;
+  const location = useLocation();
+  if (location.pathname === "/login") return;
   return (
     <header>
       <NavLink className="logo" to="/">
         Quiz Uz
       </NavLink>
       <nav>
-        <NavLink className="profile" to="/profile">
-          DA
-        </NavLink>
+        {location.pathname !== "/" ? (
+          <NavLink className="profile" to="/profile">
+            DA
+          </NavLink>
+        ) : (
+          <Link className="LoginBtn" to="/login">
+            Login
+          </Link>
+        )}
       </nav>
     </header>
   );
