ab283982ae98e57e03df47b200f5f9a6e2aca3af
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-05-27 01:05:53 +0500

    user information fetch and used redux

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Header.css";
-import { Link, NavLink, useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { newItemAction } from "../../redux/reduser";
+import { useDispatch, useSelector } from "react-redux";
 
 const Header = () => {
+  const getData = useSelector((dataReduser) => dataReduser);
+  const dispatch = useDispatch();
   const location = useLocation();
+
+  useEffect(() => {
+    fetch("http://localhost:4000/auth/getProfileInformaton", {
+      method: "GET",
+      headers: {
+        token: localStorage.getItem("authorization"),
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.statusCode === 401) {
+          dispatch(newItemAction([]));
+        }
+        dispatch(newItemAction(data));
+      });
+  }, []);
   if (location.pathname === "/login") return;
   return (
     <header>
-      <NavLink className="logo" to="/">
+      <Link className="logo" to="/">
         Quiz Uz
-      </NavLink>
+      </Link>
       <nav>
-        {location.pathname !== "/" ? (
-          <NavLink className="profile" to="/profile">
-            DA
-          </NavLink>
+        {getData?.data[0] ? (
+          <div style={{ display: "flex", alignItems: "center", gap: "20px" }}>
+            {location.pathname !== "/quizes" && (
+              <Link to="quizes" className="quizesLink">
+                Quizes
+              </Link>
+            )}
+            <Link className="profile" to="profile/information">
+              <img src={getData?.data[0]?.photo} alt="" />
+            </Link>
+          </div>
         ) : (
-          <Link className="LoginBtn" to="/login">
+          <Link className="LoginBtn" to="login">
             Login
           </Link>
         )}
