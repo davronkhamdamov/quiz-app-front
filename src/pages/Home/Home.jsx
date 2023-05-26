aaf7ddb27dc213f49dadea84b0bcecfd283d78c0
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-05-27 01:07:37 +0500

    Home initialize code added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "60vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>Bu home page bolishi kerak edi</h1>
+    </div>
+  );
 };
 
 export default Home;
